d5cc90b9ab04bcb4d8babb401b9af8cd2c290a60
dennis kimani <denniskimani416@gmail.com>
2023-03-29 22:46:56 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data);
+      renderBooks(data);
+      return data;
+    })
+    .catch(function(error) {
+      console.log('Error fetching books:', error);
+    });
 }
+
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
